10faf824d6c496c24d7a6ae36f441e6dfba395cc
Lucas Leung <66652566+mrworldwide1@users.noreply.github.com>
2023-06-27 13:05:49 -0400

    hyfthtyfhyftnfghjvtesbgr

--- script.js
+++ script.js
@@ -387,7 +387,7 @@ function titleScreenButtons() {
 }
 
 // Perform money earning calculation and increment money
-function MoneyPerSecond(){
+function moneyPerSecond(){
   if (storedMillis + 100 <= millis()){
     moneyIncrease = MpS / 10;
     money += moneyIncrease;
