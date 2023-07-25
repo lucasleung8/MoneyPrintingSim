4a7dbfd037eb86a49296745dcfae1249a1f41087
Lucas Leung <66652566+mrworldwide1@users.noreply.github.com>
2023-07-24 20:43:57 -0400

    htr

--- game.js
+++ game.js
@@ -772,7 +772,6 @@ function setup() {
 //-----------------------Main Sketch-----------------------//
 function draw() {
   background(bgColor);
-  achievementFunction();
 
   // Change game screen according to whatever menu option is chosen by player (play, about, exit, stats)
   if (gameState == 0) {
