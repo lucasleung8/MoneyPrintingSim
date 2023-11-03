b55799fe84ea2dd45e2532be093fd6b363fac286
Lucas Leung <66652566+mrworldwide1@users.noreply.github.com>
2023-11-03 12:09:26 -0400

    Update game.js

--- game.js
+++ game.js
@@ -782,6 +782,7 @@ function draw() {
   background(bgColor);
   achievementFunction();
   print(clickTextList);
+  
   // Change game screen according to whatever menu option is chosen by player (play, about, exit, stats)
   if (gameState == 0) {
     titleScreen();
